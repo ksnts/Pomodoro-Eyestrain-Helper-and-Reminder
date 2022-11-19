10fc3e2e849c30be0b688a8c610698119bb21b3b
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-11-19 20:18:22 +0800

    Added eyestrain reminder to the gui menu

--- Pomodoro-Eyestrain.py
+++ Pomodoro-Eyestrain.py
@@ -1,23 +1,27 @@
-#import time
+import time
 from tkinter import *
-#from plyer import notification
+from plyer import notification
+
+def eyeRest():
+        while True:
+                notification.notify(
+                app_icon ="C:/Users/santo/Desktop/Python A/Ass 4/icon.ico",
+                title = "Please rest your eyes",
+                message = "20 min reminder to look at something 20 meters away for 20 seconds",
+                timeout=10
+                )
+                time.sleep(30)
 
 menuGUI = Tk()
 
 menuOptions = Menu(menuGUI)
 menuGUI.config(menu=menuOptions)
 
-option1 = Menu(menuOptions)
+option1 = Menu(menuOptions,tearoff=0)
 menuOptions.add_cascade(label="Button 1",menu=option1)
+option1.add_command(label="Eye rest",command=eyeRest)
 
 menuGUI.mainloop()
 
-#while True:
-        #notification.notify(
-                #app_icon ="C:/Users/santo/Desktop/Python A/Ass 4/icon.ico",
-                #title = "Please rest your eyes",
-               # message = "20 min reminder to look at something 20 meters away for 20 seconds",
-                #timeout=10
-                #)
-        #time.sleep(30)
+
 
