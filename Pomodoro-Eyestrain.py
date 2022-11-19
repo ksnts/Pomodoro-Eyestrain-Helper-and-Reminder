7ded92cf99550883216989beace56326890a18c6
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-11-19 20:01:49 +0800

    Adding gui to the timer

--- Pomodoro-Eyestrain.py
+++ Pomodoro-Eyestrain.py
@@ -1,12 +1,23 @@
-import time
-from plyer import notification
+#import time
+from tkinter import *
+#from plyer import notification
 
-while True:
-        notification.notify(
-                app_icon ="C:/Users/santo/Desktop/Python A/Ass 4/icon.ico",
-                title = "Please rest your eye",
-                message = "20 min reminder to look at something 20 meters away for 20 seconds",
-                timeout=10
-                )
-        time.sleep(30)
+menuGUI = Tk()
+
+menuOptions = Menu(menuGUI)
+menuGUI.config(menu=menuOptions)
+
+option1 = Menu(menuOptions)
+menuOptions.add_cascade(label="Button 1",menu=option1)
+
+menuGUI.mainloop()
+
+#while True:
+        #notification.notify(
+                #app_icon ="C:/Users/santo/Desktop/Python A/Ass 4/icon.ico",
+                #title = "Please rest your eyes",
+               # message = "20 min reminder to look at something 20 meters away for 20 seconds",
+                #timeout=10
+                #)
+        #time.sleep(30)
 
