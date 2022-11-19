654c766bf80539e96cc79cf5453ece6cd2b5abf4
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-11-19 19:50:03 +0800

    Added timer loop

--- Pomodoro-Eyestrain.py
+++ Pomodoro-Eyestrain.py
@@ -1,12 +1,12 @@
 import time
-
 from plyer import notification
 
-notification.notify(
-    app_icon ="C:/Users/santo/Desktop/Python A/Ass 4/icon.ico",
-    title = "Please rest your eye",
-    message = "20 min reminder to look at something 20 meters away for 20 seconds",
-    timeout=10
-               
-    )
+while True:
+        notification.notify(
+                app_icon ="C:/Users/santo/Desktop/Python A/Ass 4/icon.ico",
+                title = "Please rest your eye",
+                message = "20 min reminder to look at something 20 meters away for 20 seconds",
+                timeout=10
+                )
+        time.sleep(30)
 
